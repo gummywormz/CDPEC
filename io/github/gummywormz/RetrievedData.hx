e3e6d07b65a5519a502396b25abb3d5bd8808b9c
Paul Alves <gummywormz@users.noreply.github.com>
2015-03-13 09:44:32 -0500

    Create RetrievedData.hx

---
+++ io/github/gummywormz/RetrievedData.hx
@@ -0,0 +1,44 @@
+package io.github.gummywormz;
+/**
+ * Represents retrieved data
+ * @author Paul Alves
+ */
+class RetrievedData
+{
+
+  var _name : String;
+  var _type : String;
+  var _value : String;
+
+  public function new(name:String,type:String,value:String)
+  {
+    _name = name;
+    _type = type;
+    _value = value;
+  }
+  
+  public function getName()
+  {
+    return _name;
+  }
+  
+  public function getValueAsString()
+  {
+    return _value;
+  }
+  
+  public function getTypeAsString()
+  {
+    return _type;
+  }
+  
+  public function getValueAsType()
+  {
+    if(_type=="String"){return _value;}
+    if(_type=="Float"){return Std.parseFloat(_value);}
+    if(_type=="Integer"){return Std.parseInt(_value);}
+  }
+  
+  
+
+}
