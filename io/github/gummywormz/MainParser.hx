dddb1546452e867b7a399d0998baa48e4a161878
Paul Alves <gummywormz@users.noreply.github.com>
2015-03-17 11:10:55 -0500

    Update MainParser.hx

--- io/github/gummywormz/MainParser.hx
+++ io/github/gummywormz/MainParser.hx
@@ -3,6 +3,8 @@ package io.github.gummywormz;
  * Main parser object
  * @author Paul Alves
  */
+
+ Using StringTools;
 class MainParser
 {
 	
@@ -20,5 +22,15 @@ class MainParser
 	public function addSection(s:Section)
 	{
 		_sections.add(s);
+	}
+
+	public function parseFile(f:String)
+	{
+		var lSplit = ~/\n?\r;
+		var list = lSplit.split(f);
+		for (e in f)
+		{
+			if (e.startsWith("@") || e.trim()=="")
+		}
 	}
 }
